0b6f97857e0ffbb2e1ad73f0bebeb02b5048b375
leggen-assis <souzaferreira437@gmail.com>
2024-11-08 14:29:22 -0300

    graph input with order 0 abort the code with -1

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,16 @@
 int main(int argc, char** argv) {
     if (argc > 7) {
         Graph graph("graph.txt", false);
+        
+        if (graph.getOrder() == 0)
+        	return -1;
+        	
         // graph, populationSize, genesSize, generations, heuristic, mutation rate, elitism rate, numberOfAnts, iterations, 
         TripleRomanDomination* drd = new TripleRomanDomination(graph, std::stoi(argv[1]), graph.getOrder(), std::stoi(argv[2]), std::stoi(argv[3]),
                 std::stof(argv[4]), std::stof(argv[5]), std::stoi(argv[6]), std::stoi(argv[7])); 
         std::cout << "Triple Roman Domination Number computed by Genetic Algorithm: " << drd->getGamma3rGeneticAlgorithm() << std::endl;
         std::cout << "Triple Roman Domination Number computed by ACO: " << drd->getGamma3rACO() << std::endl;
         
-
         std::cout << "\nGenetic Algorithm solution: " << std::endl;
         for (const auto& it: drd->getSolutionGeneticAlgorithm())
            std::cout << it << " ";
