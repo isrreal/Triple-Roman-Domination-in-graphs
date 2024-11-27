8d20f29c4507ff29a3aab933b616ce385dbb3adc
leggen-assis <souzaferreira437@gmail.com>
2024-11-27 11:07:20 -0300

    bash scripts updated, now do not put RVNS folder

--- scripts/automate_run_program.sh
+++ scripts/automate_run_program.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 folder_array=(
-  "heuristic1_without_RVNS 1 0"
-  "heuristic1_with_RVNS 1 1"  
-  "heuristic2_without_RVNS 2 0"
-  "heuristic2_with_RVNS 2 1"
-  "heuristic3_without_RVNS 3 0"
-  "heuristic3_with_RVNS 3 1"
+  "heuristic1 1" 
+  "heuristic2 2"
+  "heuristic3 3"
+  "mixedHeuristics 4"
 )
 
 for line in "${folder_array[@]}"
 do
-    read -r folder heuristic rvns <<< "$line"
-    ./run_program.sh -f "$folder" -h "$heuristic" -R "$rvns"
+    read -r folder heuristic <<< "$line"
+    ./run_program.sh -f "$folder" -h "$heuristic"
 done
 
 
--- scripts/run_program.sh
+++ scripts/run_program.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-while getopts "f:h:R:" opt; do
+while getopts "f:h:" opt; do
     case $opt in
         f) folder_name="$OPTARG" ;;
-        h) first_number="$OPTARG" ;;
-        R) second_number="$OPTARG" ;;
-        *) echo "Usage: $0 -f <folder_name> -h <GA_heuristic> -R <GA_with_RNVS_or_no>"; exit 1 ;;
+        h) heuristic="$OPTARG" ;;
+        *) echo "Usage: $0 -f <folder_name> -h <GA_heuristic>" ; exit 1 ;;
     esac
 done
 
-# -z verify if the string is empty
-if [[ -z "$folder_name" || -z "$first_number" || -z "$second_number" ]]; then
-    echo "Usage: $0 -f <folder_name> -h <first_number> -r <second_number>"
+if [[ -z "$folder_name" || -z "$heuristic" ]]; then
+    echo "Usage: $0 -f <folder_name> -h <first_number>"
     exit 1
 fi
 
@@ -32,7 +30,7 @@ for input_file in "$input_directory"/*.txt; do
     	# basename, in this case, removes .txt extension in the file $input_file
         output_file="$tests_directory/$(basename "$input_file" .txt)_output.csv"
 		
-        $executable "$input_file" "$(basename "$input_file" .txt)" "$first_number" "$second_number" > "$output_file"
+        $executable "$input_file" "$(basename "$input_file" .txt)" "$heuristic" > "$output_file"
 
         echo "Results written to: $output_file"
     else
