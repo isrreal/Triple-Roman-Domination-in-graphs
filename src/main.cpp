cf7aeeb69ef94d63c44b10e7b11097f9dbbeff59
leggen-assis <souzaferreira437@gmail.com>
2024-12-12 18:31:28 -0300

    code was reoganizated

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 
 int computeRightLowerBound(const Graph& graph, int lowerBound) {
     lowerBound = -1; 
-    if (graph.getMaxDegree() >= 3 && graph.getOrder() >= 2) 
+    if (graph.getMaxDegree() >= 3 && graph.getOrder() >= 2) {
         lowerBound = std::ceil(static_cast<size_t>(4.0 * graph.getOrder() / (graph.getMaxDegree() + 1.0)));
-
+	}
+	
     return lowerBound;
 }
 
@@ -17,23 +18,27 @@ int computeRightUpperBound(Graph& graph, int upperBound) {
     upperBound = -1;
     auto components { graph.connectedComponents() };
     
-    if(components.size() == 1 && graph.getMinDegree() >= 2)
+    if(components.size() == 1 && graph.getMinDegree() >= 2) {
         upperBound = std::floor(3.0 * graph.getOrder() / 2.0);
-    else if (components.size() == 1 && graph.getOrder() >= 3)  
+    }
+    else if (components.size() == 1 && graph.getOrder() >= 3) {
         upperBound = std::floor(7.0 * graph.getOrder() / 4.0);
+    }
     else if (components.size() > 1 && graph.getOrder() >= 3) {
         upperBound = 0;
         for (auto& par: components) {
-            if(par.first == 1) 
+            if(par.first == 1) {
                 upperBound += 3;
-            else if(par.first == 2) 
+            }
+            else if(par.first == 2) {
                 upperBound += 4;
-            
-            else if(par.first >= 3 && par.second >= 2) 
+            }            
+            else if(par.first >= 3 && par.second >= 2) {
                 upperBound += std::floor(static_cast<size_t>(3.0 * par.first / 2.0));
-           
-            else if(par.first >= 3) 
+            }           
+            else if(par.first >= 3) {
                 upperBound += std::floor(static_cast<size_t>(7.0 * par.first / 4.0));
+            }
         }
     }
 
@@ -44,28 +49,30 @@ int main(int argc, char** argv) {
     if (argc > 3) {
         Graph graph(argv[1], false);
         
-        if (graph.getOrder() == 0)
+        if (graph.getOrder() == 0) {
             return -1;
+        }
         
-    	constexpr size_t trial {10};
-        size_t populationSize { static_cast<size_t>(graph.getOrder() / 1.5) };
-        size_t generations {1000};
+    	constexpr size_t trial = 10;
+        size_t populationSize = static_cast<size_t>(graph.getOrder() / 1.5);
+        size_t generations = 1000;
         short heuristic = std::stoi(argv[3]);
-        constexpr float mutationRate { 0.2 };
-        constexpr float elitismRate { 0.1 };
-        constexpr float crossOverRate { 0.7 };
-        constexpr size_t numberOfAnts { 5 };
-        constexpr size_t iterations { 10 };
+        constexpr float mutationRate = 0.2;
+        constexpr float elitismRate = 0.1;
+        constexpr float crossOverRate = 0.7;
+        constexpr size_t numberOfAnts = 5;
+        constexpr size_t iterations = 10;
         
         int upperBound { 0 };
     	int lowerBound { 0 };
         
-        size_t Delta { graph.getMaxDegree() };
-       	size_t delta { graph.getMinDegree() };
+        size_t Delta = graph.getMaxDegree();
+       	size_t delta = graph.getMinDegree();
        	
        // graph, populationSize, genesSize, generations, heuristic, mutation rate, elitism rate, numberOfAnts, iterations
-        TripleRomanDomination* trd = new TripleRomanDomination(graph, populationSize, graph.getOrder(), generations,
-                mutationRate, elitismRate, crossOverRate, numberOfAnts, iterations); 
+        TripleRomanDomination trd(graph, populationSize, graph.getOrder(), generations,
+                mutationRate, elitismRate, crossOverRate, numberOfAnts, iterations);
+                 
         std::cout << "graph_name,graph_order,graph_size,graph_min_degree,graph_max_degree,GA_fitness_heuristic" << heuristic;
 	    std::cout << ",ACO_fitness_" << numberOfAnts << "_" << iterations; 
         std::cout << ",lower_bound,upper_bound,elapsed_time_GA(seconds)" << ",elapsed_time_ACO(seconds),is_3RDF_GA,is_3RDF_ACO\n";
@@ -79,8 +86,7 @@ int main(int argc, char** argv) {
 	        
 	        std::chrono::duration<double> elapsedGA;
 	    	std::chrono::duration<double> elapsedACO;
-	    	
-	
+	    		
 	    	std::thread gaThread([&]() {
 	    		auto startGA = std::chrono::high_resolution_clock::now();
 	    		trd->runGeneticAlgorithm(heuristic);
@@ -112,12 +118,9 @@ int main(int argc, char** argv) {
 	    	std::cout << elapsedACO.count() << ",";	    	
 	    	
 	    	std::cout << TripleRomanDomination::feasible(graph, trd->getSolutionGeneticAlgorithm()) << ",";
-	    	std::cout << TripleRomanDomination::feasible(graph, trd->getSolutionACO()) << '\n';
-	    	
-     }
-        
-	    delete trd;
-	    return EXIT_SUCCESS;
-	    
-    }
+	    	std::cout << TripleRomanDomination::feasible(graph, trd->getSolutionACO()) << '\n';	
+ 		}
+	}
+	
+    return EXIT_SUCCESS;
 }
