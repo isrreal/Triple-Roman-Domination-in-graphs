40e1cf4258539951e233d0b398106afa2482386f
leggen-assis <souzaferreira437@gmail.com>
2024-12-14 12:42:35 -0300

    util_functions file added: toggleVertices, feasible, feasibilityCheck, getRandomInt, getRandomFloat, fitness methods

---
+++ inc/util_functions.hpp
@@ -0,0 +1,21 @@
+#ifndef UTIL_FUNCTIONS_HPP
+#define UTIL_FUNCTIONS_HPP
+
+#include <vector>
+#include <random>
+#include "Graph.hpp"
+#include "Chromosome.hpp"
+
+size_t getRandomInt(size_t, size_t); 
+
+float getRandomFloat(float, float);
+
+bool feasible(const Graph&, const std::vector<int>&);
+
+Chromosome& feasibilityCheck(const Graph& , Chromosome&);
+
+void toggleLabels(const Graph&, std::vector<int>&);
+
+Chromosome& fitness(Chromosome&);
+
+#endif
