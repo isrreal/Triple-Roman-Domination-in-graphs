d907e28dd6b74cad6de7e1063f75da03c1fc07d1
leggen-assis <souzaferreira437@gmail.com>
2024-12-20 08:09:44 -0300

    input folder mechanism added

--- scripts/automate_run_program.sh
+++ scripts/automate_run_program.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+while getopts "f:" opt; do
+	case $opt in
+		f) input_folder_name="$OPTARG" ;;
+	esac
+done
+
 output_folder_array=(
   "heuristic1 1" 
   "heuristic2 2"
@@ -10,7 +16,7 @@ output_folder_array=(
 for line in "${output_folder_array[@]}"
 do
     read -r folder heuristic <<< "$line"
-    ./run_program.sh -i random_graphs -o "$folder" -h "$heuristic"
+    ./run_program.sh -i "$input_folder_name" -o "$folder" -h "$heuristic"
 done
 
 
